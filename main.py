d210b30485c4b40bb2bcf5cb122b2d15078df493
juliesigal <90146733+juliesigal@users.noreply.github.com>
2021-09-07 14:42:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+a = int(input("please pick up a number"))
+
+    #targil1
+#1 if a % 5 == 0 and a % 2 == 0:
+    #print("can be devided by 10")
+
+     #targil 2
+#if a == 0 or a == 1:
+   # print("0 or 1")
+#else:
+  #  if a == -1:
+ #      print("-1")
+ #   else:
+       # print("unknown number")
+
+
+        #targil 3
+#if int(a / 10) > 0 and int(a / 10) <= 9:
+      #  print(a % 10)
+
+
+      
+      #targil 4
+#asarot = int(a / 10)
+#ahadot = a % 10
+#if asarot == ahadot and asarot >1 and ahadot>1:
+  #  print("equal and above 1")
